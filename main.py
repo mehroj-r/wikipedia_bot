3284c19c632d42258c3c6388b6af93c77bf6c9e7
MEHROJ <mekhrojmajidov@gmail.com>
2023-08-31 11:20:42 +0500

    Updated main.py
    Added Wikipedia Search support

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import asyncio
 import logging
 import sys
+import wikipedia
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 from aiogram.utils.markdown import hbold
+
+# Defining Wikipedia Language
+wikipedia.set_lang("en")
 
 # Bot token can be obtained via https://t.me/BotFather
 TOKEN = "6583942031:AAFg210_atreTpKav8wRxRE84kt3ReKWVsE"
@@ -25,22 +29,18 @@ async def command_start_handler(message: Message) -> None:
     # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
     # method automatically or call API method directly via
     # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
-    await message.reply(f"Hello, {hbold(message.from_user.full_name)}!")
+    await message.reply(f"Hello, {hbold(message.from_user.full_name)}! \nWelcome to Wikipedia Bot. Send any text message to get information about.")
 
 
 @dp.message()
-async def echo_handler(message: types.Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
+async def wiki_handler(message: types.Message) -> None:
     try:
+        wiki_response = wikipedia.summary(message.text)
         # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
+        await message.answer(wiki_response)     
+    except:
         # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+        await message.answer("No wikipedia page was found for this request !!!")
 
 
 async def main() -> None:
