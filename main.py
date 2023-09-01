59cf169b94f07578514801ac3c3ea728aab1bd47
MEHROJ <mekhrojmajidov@gmail.com>
2023-09-01 18:19:07 +0500

    Updated main.py
    Avoiding character limitation issue by Telegram

--- main.py
+++ main.py
@@ -15,6 +15,18 @@ TOKEN = "BOT_TOKEN"
 # All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
 
+def cut_long_message(text):
+    sentences_list = text.split('.')[0:-1]
+
+    while True:
+        sentences_list.pop()
+        text_regen = '.'.join(sentences_list)
+        if len(text_regen) <= 4095:
+            break
+
+    text_regen = text_regen + '.'
+
+    return text_regen
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
@@ -23,24 +35,26 @@ async def command_start_handler(message: Message) -> None:
     """
     await message.reply(f"Hello, {hbold(message.from_user.full_name)}! \nWelcome to Wikipedia Bot. Send any text message to get information about.")
 
-
 @dp.message()
 async def wiki_handler(message: types.Message) -> None:
     try:
         # Send a response from Wikipedia API
         response = wiki_response(message.text)
+
+        # Cutting reponse text if it is above character limitation
+        if len(response) > 4096:
+            response = cut_long_message(response)
+
         await message.answer(response)
-    except:
+    except TypeError:
         # But not all the inputs are supported
         await message.answer("No wikipedia page was found for this request !!!")
-
 
 async def main() -> None:
     # Initialize Bot instance with a default parse mode which will be passed to all API calls
     bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
     # And the run events dispatching
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
